f4aa2a2ba06b3ea9aa009349e574dacfea8040e0
arthur-lava <97080076@st.deltion.nl>
2025-10-08 18:22:39 +0200

    fix: clean vite.config.js (remove stray @')

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-@'
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
 });
-'@ | Set-Content -Encoding UTF8 vite.config.js
