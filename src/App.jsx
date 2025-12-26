ab2dd581fe74734cd59989c2692ab3b5df64f17e
Arthurlava <65039872+Arthurlava@users.noreply.github.com>
2025-12-26 19:12:44 +0100

    Fix JSX syntax in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1000,7 +1000,6 @@ useEffect(() => {
                         </div>
                     )}
 
-                    <div className="row">
 {!room?.started && (
   <div className="row">
     <input
@@ -1057,6 +1056,7 @@ useEffect(() => {
     {!room?.started && <span className="badge">Room: <b>{roomCode}</b></span>}
   </div>
 )}
+</div>
 
 
 
